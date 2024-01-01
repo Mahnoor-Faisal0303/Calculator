9ed53d307186050041d04661a0cb92a7633a0828
mahnoor <faisalmahnoor74@gmail.com>
2024-01-02 02:23:46 +0500

    completed

--- calculator.js
+++ calculator.js
@@ -1,3 +1,38 @@
+  let DIcon = document.getElementById("darkModeIcon");
+  let WIcon =document.getElementById("whiteModeIcon");
+  document.getElementById("whiteModeIcon").addEventListener("click", function () {
+
+    document.getElementById("containerId").classList.replace("containerWhite","container");
+    document.querySelectorAll(".whitebtns").forEach(function (btn) {
+    btn.classList.replace("whitebtns", "blackbtns");
+});
+    // DIcon.classList.replace("displayNone","displayShow");
+    // WIcon.classList.replace("displayShow","displayNone");
+
+    DIcon.style.display="block";
+    WIcon.style.display="none";
+    document.getElementById("rectangle1Id").classList.replace("rectangle1White","rectangle1");
+  });
+  document.getElementById("darkModeIcon").addEventListener("click", function () {
+    document.getElementById("containerId").classList.replace("container","containerWhite");
+  document.querySelectorAll(".blackbtns").forEach(function (btn) {
+    btn.classList.replace("blackbtns", "whitebtns");
+});
+
+   
+    // DIcon.classList.add("displayNone");
+    // DIcon.classList.add("displayShow");
+    // WIcon.classList.add("displayNone");
+    // WIcon.classList.add("displayShow");
+ // DIcon.classList.replace("displayShow", "displayNone");
+    // WIcon.classList.replace("displayNone", "displayShow");
+
+    DIcon.style.display="none";
+    WIcon.style.display="block";
+    document.getElementById("rectangle1Id").classList.replace("rectangle1","rectangle1White");
+  
+  });
+
 const defaultNumElement = document.getElementById("defaultNum");
 const newNumElement = document.getElementById("newNum");
 const keysContainer = document.querySelector(".keys");
@@ -7,7 +42,9 @@ let defaultNumber = "";
 let operator = "";
 
 function clear() {
-  defaultNumElement.textContent = defaultNumber;
+  defaultNumber = "";
+  operator = "";
+  defaultNumElement.textContent = "";
   currentNumber = "";
   newNumElement.textContent = "";
 }
@@ -19,9 +56,14 @@ function numberClick(number) {
 }
 
 function operatorClick(op) {
+  if(defaultNumber!==""){
+    calculate();
+  }
   defaultNumber = currentNumber + op;
   currentNumber = "";
   operator = op;
+  defaultNumElement.textContent = defaultNumber;
+  newNumElement.textContent = currentNumber;
 }
 
 function calculate() {
@@ -51,7 +93,7 @@ function calculate() {
       break;
   }
   defaultNumber = "";
-  operator = "";
+   operator = "";
   defaultNumElement.textContent = defaultNumber;
   newNumElement.textContent = currentNumber;
 }
