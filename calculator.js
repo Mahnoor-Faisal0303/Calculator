04763beff7359348e31ca77e62de72d87b4c3d5e
mahnoor <faisalmahnoor74@gmail.com>
2024-01-02 15:51:33 +0500

    updated

--- calculator.js
+++ calculator.js
@@ -1,37 +1,31 @@
-  let DIcon = document.getElementById("darkModeIcon");
-  let WIcon =document.getElementById("whiteModeIcon");
-  document.getElementById("whiteModeIcon").addEventListener("click", function () {
-
-    document.getElementById("containerId").classList.replace("containerWhite","container");
-    document.querySelectorAll(".whitebtns").forEach(function (btn) {
+let DIcon = document.getElementById("darkModeIcon");
+let WIcon = document.getElementById("whiteModeIcon");
+document.getElementById("whiteModeIcon").addEventListener("click", function () {
+  document
+    .getElementById("containerId")
+    .classList.replace("containerWhite", "container");
+  document.querySelectorAll(".whitebtns").forEach(function (btn) {
     btn.classList.replace("whitebtns", "blackbtns");
-});
-    // DIcon.classList.replace("displayNone","displayShow");
-    // WIcon.classList.replace("displayShow","displayNone");
-
-    DIcon.style.display="block";
-    WIcon.style.display="none";
-    document.getElementById("rectangle1Id").classList.replace("rectangle1White","rectangle1");
   });
-  document.getElementById("darkModeIcon").addEventListener("click", function () {
-    document.getElementById("containerId").classList.replace("container","containerWhite");
+  DIcon.classList.replace("displayNone", "displayShow");
+  WIcon.classList.replace("displayShow", "displayNone");
+  document
+    .getElementById("rectangle1Id")
+    .classList.replace("rectangle1White", "rectangle1");
+});
+document.getElementById("darkModeIcon").addEventListener("click", function () {
+  document
+    .getElementById("containerId")
+    .classList.replace("container", "containerWhite");
   document.querySelectorAll(".blackbtns").forEach(function (btn) {
     btn.classList.replace("blackbtns", "whitebtns");
-});
-
-   
-    // DIcon.classList.add("displayNone");
-    // DIcon.classList.add("displayShow");
-    // WIcon.classList.add("displayNone");
-    // WIcon.classList.add("displayShow");
- // DIcon.classList.replace("displayShow", "displayNone");
-    // WIcon.classList.replace("displayNone", "displayShow");
-
-    DIcon.style.display="none";
-    WIcon.style.display="block";
-    document.getElementById("rectangle1Id").classList.replace("rectangle1","rectangle1White");
-  
   });
+  DIcon.classList.replace("displayShow", "displayNone");
+  WIcon.classList.replace("displayNone", "displayShow");
+  document
+    .getElementById("rectangle1Id")
+    .classList.replace("rectangle1", "rectangle1White");
+});
 
 const defaultNumElement = document.getElementById("defaultNum");
 const newNumElement = document.getElementById("newNum");
@@ -56,7 +50,7 @@ function numberClick(number) {
 }
 
 function operatorClick(op) {
-  if(defaultNumber!==""){
+  if (defaultNumber !== "") {
     calculate();
   }
   defaultNumber = currentNumber + op;
@@ -72,32 +66,34 @@ function calculate() {
 
   switch (operator) {
     case "%":
-      currentNumber = (num1 * (num2 / 100));
+      if (currentNumber == "") {
+        currentNumber = num1 / 100;
+      } else {
+         currentNumber = num1 * (num2 / 100);
+      }
       break;
     case "+":
       currentNumber = (num1 + num2);
       break;
     case "-":
-      currentNumber = (num1 - num2);
+      currentNumber = num1 - num2;
       break;
     case "x":
       currentNumber = (num1 * num2);
       break;
     case "/":
       if (num2 !== 0) {
-        currentNumber = (num1 / num2);
-      } 
-      else if(num1==0 && num2==0){
+        currentNumber = num1 / num2;
+      } else if (num1 == 0 && num2 == 0) {
         currentNumber = 0;
-      }
-      else {
+      } else {
         currentNumber = "Error";
       }
     default:
       break;
   }
   defaultNumber = "";
-   operator = "";
+  operator = "";
   defaultNumElement.textContent = defaultNumber;
   newNumElement.textContent = currentNumber;
 }
@@ -105,23 +101,30 @@ function calculate() {
 keysContainer.addEventListener("click", function (event) {
   const target = event.target;
   const keyValue = target.textContent;
-    switch (keyValue) {
-      case "AC":
-        clear();
-        break;
-      case "=":
-        calculate();
-        break;
-      case "%":
-      case "+":
-      case "-":
-      case "x":
-      case "/":
-        operatorClick(keyValue);
-        break;
-      default:
-        numberClick(keyValue);
-        break;
-    }
+  switch (keyValue) {
+    case "AC":
+      clear();
+      break;
+    case "=":
+      calculate();
+      break;
+    case "%":
+      operatorClick(keyValue);
+      break;
+    case "+":
+      operatorClick(keyValue);
+      break;
+    case "-":
+      operatorClick(keyValue);
+      break;
+    case "x":
+      operatorClick(keyValue);
+      break;
+    case "/":
+      operatorClick(keyValue);
+      break;
+    default:
+      numberClick(keyValue);
+      break;
   }
-);
+});
